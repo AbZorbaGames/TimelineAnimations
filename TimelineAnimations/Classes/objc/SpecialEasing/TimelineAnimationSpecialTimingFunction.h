9b1475ad703e0fd0a3a34b389aca7eba80c13487
Georges Boumis <boumis@abzorbagames.com>
2017-12-12 14:44:14 +0200

    bring back special timing functions

--- TimelineAnimations/Classes/objc/SpecialEasing/TimelineAnimationSpecialTimingFunction.h
+++ TimelineAnimations/Classes/objc/SpecialEasing/TimelineAnimationSpecialTimingFunction.h
@@ -19,45 +19,45 @@ extern "C" {
     // Linear interpolation (no easing)
     double LinearInterpolation(double p);
 
-//    // Quadratic easing; p^2
-//    double QuadraticEaseIn(double p);
-//    double QuadraticEaseOut(double p);
-//    double QuadraticEaseInOut(double p);
-//
-//    // Cubic easing; p^3
-//    double CubicEaseIn(double p);
-//    double CubicEaseOut(double p);
-//    double CubicEaseInOut(double p);
-//
-//    // Quartic easing; p^4
-//    double QuarticEaseIn(double p);
-//    double QuarticEaseOut(double p);
-//    double QuarticEaseInOut(double p);
-//
-//    // Quintic easing; p^5
-//    double QuinticEaseIn(double p);
-//    double QuinticEaseOut(double p);
-//    double QuinticEaseInOut(double p);
-//
-//    // Sine wave easing; sin(p * PI/2)
-//    double SineEaseIn(double p);
-//    double SineEaseOut(double p);
-//    double SineEaseInOut(double p);
-//
-//    // Circular easing; sqrt(1 - p^2)
-//    double CircularEaseIn(double p);
-//    double CircularEaseOut(double p);
-//    double CircularEaseInOut(double p);
-//
-//    // Exponential easing, base 2
-//    double ExponentialEaseIn(double p);
-//    double ExponentialEaseOut(double p);
-//    double ExponentialEaseInOut(double p);
-//
-//    // Overshooting cubic easing;
-//    double BackEaseIn(double p);
-//    double BackEaseOut(double p);
-//    double BackEaseInOut(double p);
+    // Quadratic easing; p^2
+    double QuadraticEaseIn(double p);
+    double QuadraticEaseOut(double p);
+    double QuadraticEaseInOut(double p);
+
+    // Cubic easing; p^3
+    double CubicEaseIn(double p);
+    double CubicEaseOut(double p);
+    double CubicEaseInOut(double p);
+
+    // Quartic easing; p^4
+    double QuarticEaseIn(double p);
+    double QuarticEaseOut(double p);
+    double QuarticEaseInOut(double p);
+
+    // Quintic easing; p^5
+    double QuinticEaseIn(double p);
+    double QuinticEaseOut(double p);
+    double QuinticEaseInOut(double p);
+
+    // Sine wave easing; sin(p * PI/2)
+    double SineEaseIn(double p);
+    double SineEaseOut(double p);
+    double SineEaseInOut(double p);
+
+    // Circular easing; sqrt(1 - p^2)
+    double CircularEaseIn(double p);
+    double CircularEaseOut(double p);
+    double CircularEaseInOut(double p);
+
+    // Exponential easing, base 2
+    double ExponentialEaseIn(double p);
+    double ExponentialEaseOut(double p);
+    double ExponentialEaseInOut(double p);
+
+    // Overshooting cubic easing;
+    double BackEaseIn(double p);
+    double BackEaseOut(double p);
+    double BackEaseInOut(double p);
 
     // Exponentially-damped sine wave easing
     double ElasticEaseIn(double p);
